3891b92fc3c1af1749c1897e281058f47236e334
daninger4995 <56229990+daninger4995@users.noreply.github.com>
2020-05-30 09:26:33 -0700

    setting up discord part

--- logstream.py
+++ logstream.py
@@ -5,12 +5,10 @@ import praw
 reddit = praw.Reddit(username = config.username,password = config.password,client_id = config.id,client_secret = config.secret,user_agent = config.user_agent)
 subreddit = reddit.subreddit(config.sub)
 
-
 for log in reddit.subreddit("mod").mod.log(limit=1000):
-    if log.mod.name in config.name and if
-        print("Mod: {}, Action: {}, Link: {}".format(log.mod, log.action, log.target_permalink))
+    if log.mod.name in config.name and log.action in config.actiontype:
+            print("Mod: {}, Action: {}, Link: {}".format(log.mod, log.action, log.target_permalink))
 
     if log.mod.name not in config.name:
         continue
 
-subreddit = reddit.subreddit(config.sub)
