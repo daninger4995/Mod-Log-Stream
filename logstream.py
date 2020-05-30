75ae475716b72136425c5914c4f0f01cf91b071a
daninger4995 <56229990+daninger4995@users.noreply.github.com>
2020-05-29 22:59:14 -0700

    update, need to figure out stream

--- logstream.py
+++ logstream.py
@@ -5,8 +5,12 @@ import praw
 reddit = praw.Reddit(username = config.username,password = config.password,client_id = config.id,client_secret = config.secret,user_agent = config.user_agent)
 subreddit = reddit.subreddit(config.sub)
 
-for log in reddit.subreddit("mod").mod.log(limit=5):
-    if log.mod in config.mods:
+
+for log in reddit.subreddit("mod").mod.log(limit=1000):
+    if log.mod.name in config.name:
         print("Mod: {}, Action: {}, Link: {}".format(log.mod, log.action, log.target_permalink))
+
+    if log.mod.name not in config.name:
+        print("Not Found")
 
 subreddit = reddit.subreddit(config.sub)
