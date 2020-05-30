613d936e49bd71c9241276f908cf39cb94aa5ea3
daninger4995 <56229990+daninger4995@users.noreply.github.com>
2020-05-30 09:53:43 -0700

    setting up discord part

--- streamtodiscord.py
+++ streamtodiscord.py
@@ -1,7 +1,12 @@
 import discord
 
-client = discord.Client()\
+def main():
+	client.run(config.discord_id)
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
     print("The bot is ready!")
+    await client.change_presence(game=discord.Game(name="Snitchin on you fools yeetyeet))
+
