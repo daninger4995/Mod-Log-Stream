45fa9ef1270ba38d82cd5aa35795d41d0808f43a
daninger4995 <56229990+daninger4995@users.noreply.github.com>
2020-05-30 18:30:09 -0700

    Added example config

---
+++ config_example.py
@@ -0,0 +1,13 @@
+id = "REDDIT_CLIENT_id"
+secret = "REDDIT_SECRET"
+username = "REDDIT_USERNAME"
+password = "REDDIT_PASSWORD"
+user_agent = "Modlog Streamer by u/daninger4995 and u/terminator076 v1.0"
+sub = "SUBREDDIT"
+name = {'MOD1', 'MOD2', 'MOD3'}
+#Add other actions below if needed. Go to /r/SUBREDDIT/about/log.json to see what options are visible
+actiontype = {'approvelink', 'removelink', 'approvecomment', 'removecomment', 'banuser'}
+
+discord_id = "BOT_ID"
+channel_id = CHANNEL_ID
+owner_id = BOT_OWNER_ID
